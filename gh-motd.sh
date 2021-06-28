5555e40d2f0161e4ad347005e5780bc378ada9f9
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2021-06-28 11:58:25 +0300

    Update gh-motd.sh

--- gh-motd.sh
+++ gh-motd.sh
@@ -5,21 +5,23 @@
 #--------------------------------------------------
 echo -e "\nMOTD Configurations"
 echo -e "\n---Installing ansiweather, lolcat, and sysvbanner"
-sudo apt-get update && sudo apt-get install ansiweather lolcat sysvbanner -y &> /dev/null
+sudo apt-get update &> /dev/null && sudo apt-get install ansiweather lolcat sysvbanner -y &> /dev/null
 echo -e "\n---Creating MOTD Config"
-sudo bash -c 'cat <<EOF > /etc/update-motd.d/00-welcome
+sudo dd of=/etc/update-motd.d/00-welcome << EOF
 #!/bin/sh
 echo
 sysvbanner "$(hostname -s)" | /usr/games/lolcat -f
+echo
+/usr/bin/neofetch --ascii_distro GNU
 echo
 /usr/bin/landscape-sysinfo
 echo
 stamp="/var/lib/update-notifier/updates-available"
 [ ! -r "$(stamp)" ] || cat "$(stamp)"
-find $(stamp) -newermt "now-7 days"" 2> /dev/null | grep -q -m 1 "." || /usr/share/update-notifier/notify-updates-outdated
+find $(stamp) -newermt 'now-7 days' 2> /dev/null | grep -q -m 1 '.' || /usr/share/update-notifier/notify-updates-outdated
 ansiweather -l Kijabe,KE -p false
 ansiweather -l Kijabe,KE -p false -f 3
 echo
-EOF'
+EOF
 sudo chmod -x /etc/update-motd.d/* && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
 echo -e "\n---MOTD Finished\n"
