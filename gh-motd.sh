f5105d988377d4a8ce04d79b6dfdec03753b6ad7
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2021-06-28 11:40:50 +0300

    Update gh-motd.sh

--- gh-motd.sh
+++ gh-motd.sh
@@ -4,24 +4,19 @@
 # MOTD Configurations
 #--------------------------------------------------
 echo -e "\nMOTD Configurations"
-echo -e "\n---Adding neofetch Repository"
-sudo add-apt-repository ppa:dawidd0811/neofetch -y &> /dev/null
-yes '' | sudo add-apt-repository ppa:nginx/stable
-echo -e "\n---Installing neofetch, ansiweather, lolcat, and sysvbanner"
-sudo apt-get update && sudo apt-get install neofetch ansiweather lolcat sysvbanner -y &> /dev/null
+echo -e "\n---Installing ansiweather, lolcat, and sysvbanner"
+sudo apt-get update && sudo apt-get install ansiweather lolcat sysvbanner -y &> /dev/null
 echo -e "\n---Creating MOTD Config"
 sudo bash -c 'cat <<EOF > /etc/update-motd.d/00-welcome
 #!/bin/sh
 echo
-sysvbanner $(hostname -s) | /usr/games/lolcat -f
-echo
-#/usr/bin/neofetch --ascii_distro GNU
+sysvbanner "$(hostname -s)" | /usr/games/lolcat -f
 echo
 /usr/bin/landscape-sysinfo
 echo
 stamp="/var/lib/update-notifier/updates-available"
-[ ! -r "$stamp" ] || cat "$stamp"
-find $stamp -newermt 'now-7 days' 2> /dev/null | grep -q -m 1 '.' || /usr/share/update-notifier/notify-updates-outdated
+[ ! -r "$(stamp)" ] || cat "$(stamp)"
+find $(stamp) -newermt "now-7 days"" 2> /dev/null | grep -q -m 1 "." || /usr/share/update-notifier/notify-updates-outdated
 ansiweather -l Kijabe,KE -p false
 ansiweather -l Kijabe,KE -p false -f 3
 echo
