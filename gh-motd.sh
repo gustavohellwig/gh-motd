b17073aafafa46583be5ac2fc4b08109a3a5c83a
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2021-06-28 12:31:42 +0300

    Update gh-motd.sh

--- gh-motd.sh
+++ gh-motd.sh
@@ -6,22 +6,7 @@
 echo -e "\nMOTD Configurations"
 echo -e "\n---Installing ansiweather, lolcat, and sysvbanner"
 sudo apt-get update &> /dev/null && sudo apt-get install ansiweather lolcat sysvbanner -y &> /dev/null
-echo -e "\n---Creating MOTD Config"
-sudo dd of=/etc/update-motd.d/00-welcome << EOF
-#!/bin/sh
-echo
-sysvbanner "$(hostname -s)" | /usr/games/lolcat -f
-echo
-/usr/bin/neofetch --ascii_distro GNU
-echo
-/usr/bin/landscape-sysinfo
-echo
-stamp="/var/lib/update-notifier/updates-available"
-[ ! -r "$(stamp)" ] || cat "$(stamp)"
-find $(stamp) -newermt 'now-7 days' 2> /dev/null | grep -q -m 1 '.' || /usr/share/update-notifier/notify-updates-outdated
-ansiweather -l Kijabe,KE -p false
-ansiweather -l Kijabe,KE -p false -f 3
-echo
-EOF
+echo -e "\n---Downloading MOTD Config"
+sudo wget https://raw.githubusercontent.com/gustavohellwig/gh-motd/main/00-welcome -P /etc/update-motd.d/ &> /dev/null && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
 sudo chmod -x /etc/update-motd.d/* && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
 echo -e "\n---MOTD Finished\n"
