6a8430172256106fbffc9e0bcc56fed97b7b4f7e
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2023-03-16 13:34:39 +0300

    Update gh-motd.sh

--- gh-motd.sh
+++ gh-motd.sh
@@ -5,7 +5,7 @@
 #--------------------------------------------------
 echo -e "\nMOTD Configurations"
 echo -e "\n---Installing ansiweather, lolcat, and sysvbanner"
-sudo apt-get update &> /dev/null && sudo apt-get install ansiweather lolcat sysvbanner -y &> /dev/null
+sudo apt-get update &> /dev/null && sudo apt-get install ansiweather lolcat sysvbanner figlet -y &> /dev/null
 echo -e "\n---Downloading MOTD Config"
 sudo wget https://raw.githubusercontent.com/gustavohellwig/gh-motd/main/00-welcome -P /etc/update-motd.d/ &> /dev/null && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
 sudo chmod -x /etc/update-motd.d/* && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
