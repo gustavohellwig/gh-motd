77073ead14133df9425f86f14ed8ec6d5321ef86
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2023-03-16 13:36:55 +0300

    Update gh-motd.sh

--- gh-motd.sh
+++ gh-motd.sh
@@ -10,3 +10,5 @@ echo -e "\n---Downloading MOTD Config"
 sudo wget https://raw.githubusercontent.com/gustavohellwig/gh-motd/main/00-welcome -P /etc/update-motd.d/ &> /dev/null && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
 sudo chmod -x /etc/update-motd.d/* && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
 echo -e "\n---MOTD Finished\n"
+echo -e "\nUpdate the file /etc/update-motd.d/00-welcome according the location that you want to show the weather forecast"
+echo -e "\n---MOTD---\n"
