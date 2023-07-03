d02f1ffb0a2cc3a8aff2143dfa96d93367ff805d
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2023-07-03 13:55:57 +0300

    Update gh-motd.sh

--- gh-motd.sh
+++ gh-motd.sh
@@ -7,7 +7,7 @@ echo -e "\nMOTD Configurations"
 echo -e "\n---Installing ansiweather, lolcat, and sysvbanner"
 sudo apt-get update &> /dev/null && sudo apt-get install ansiweather lolcat sysvbanner figlet -y &> /dev/null
 echo -e "\n---Downloading MOTD Config"
-sudo wget https://raw.githubusercontent.com/gustavohellwig/gh-motd/main/00-welcome -P /etc/update-motd.d/ &> /dev/null && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
+sudo rm /etc/update-motd.d/00-welcome &> /dev/null && sudo wget https://raw.githubusercontent.com/gustavohellwig/gh-motd/main/00-welcome -P /etc/update-motd.d/ &> /dev/null && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
 sudo chmod -x /etc/update-motd.d/* && sudo chmod +x /etc/update-motd.d/00-welcome &> /dev/null
 echo -e "\n---MOTD Finished---\n"
 echo -e "Update this file \"/etc/update-motd.d/00-welcome\" according to the location where you want to show the weather forecast."
