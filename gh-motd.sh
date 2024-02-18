5a0476dd28461e622cccdf83b1b7c24b8d49909c
Gustavo Hellwig <7680647+gustavohellwig@users.noreply.github.com>
2024-02-18 06:11:18 +0300

    Update gh-motd.sh

--- gh-motd.sh
+++ gh-motd.sh
@@ -4,14 +4,15 @@
 # MOTD Configurations
 #--------------------------------------------------
 echo -e "\nMOTD Configurations"
-echo -e "\n---Installing ansiweather, lolcat, and sysvbanner"
-sudo apt-get update
-sudo apt-get install ansiweather lolcat sysvbanner figlet -y
-echo -e "\n---Downloading MOTD Config"
-sudo rm /etc/update-motd.d/00-welcome
-sudo wget https://raw.githubusercontent.com/gustavohellwig/gh-motd/main/00-welcome -P /etc/update-motd.d/
-sudo chmod -x /etc/update-motd.d/*
-sudo chmod +x /etc/update-motd.d/00-welcome
-echo -e "\n---MOTD Finished---\n"
-echo -e "Update this file \"/etc/update-motd.d/00-welcome\" according to the location where you want to show the weather forecast."
+echo -e "\n→ Installing ansiweather, lolcat, and sysvbanner\n\n↳ Type your SUDO password:"
+sudo apt-get update &>/dev/null
+sudo apt-get install ansiweather lolcat sysvbanner figlet -y &>/dev/null
+echo -e "\n→ Downloading MOTD config template"
+sudo rm /etc/update-motd.d/00-welcome &>/dev/null
+sudo wget https://raw.githubusercontent.com/gustavohellwig/gh-motd/main/00-welcome &>/dev/null
+sudo mv 00-welcome /etc/update-motd.d/ &>/dev/null
+sudo chmod -x /etc/update-motd.d/* &>/dev/null
+sudo chmod +x /etc/update-motd.d/00-welcome &>/dev/null
+echo -e "\n→ MOTD Finished---\n"
+echo -e "Update this file \"/etc/update-motd.d/00-welcome\" with the information of the location you want to show the weather forecast."
 echo -e "\n"
